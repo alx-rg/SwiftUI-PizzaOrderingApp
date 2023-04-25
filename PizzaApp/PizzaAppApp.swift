ec5a663cd8964a486fea7d9fd57509959327c87b
Alex Ross <alxrg@me.com>
2023-04-25 15:37:07 -0500

    Initial Commit

---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by Alexandre Ross-Gautron on 2023-04-25.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
